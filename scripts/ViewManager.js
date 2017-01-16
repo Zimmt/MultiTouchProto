55e0db117510a6a4f7905c1611cedf105bfb0ebc
Zimbel <nina.zimbel@web.de>
2017-01-16 14:15:07 +0100

    delete onClick

--- scripts/ViewManager.js
+++ scripts/ViewManager.js
@@ -71,13 +71,15 @@ function ViewManager(look) {
 	var self = this;
 	
 	this.look = look;
-	this.svg = d3.select("#graph").append("svg") //TODO listen on group?
+	this.svg = d3.select("#graph").append("svg")
 		.attr("id", "canvas")
 		.attr(this.look.canvas.attr)
 		.style(this.look.canvas.style)
-		.on("click", function(d,i) {
-			console.log("click " + d3.event.pageX + " "+d3.event.pageY);
-			self.addNode(d3.event.pageX, d3.event.pageY);
+		.on("MosRotateGesture", function(d,i) {
+			console.log("rotate gesture");
+		})
+		.on("MozMagnifyGesture", function(d,i) {
+			console.log("magnify gesture");
 		});
 	
 	this.interactionView = new InteractionView(this.svg, this.look);
@@ -95,6 +97,11 @@ function ViewManager(look) {
 				var p = d3.mouse(self.svg.node());
 				console.log("start " + p[0] + " " + p[1] );
 				self.dragcount++;
+				
+				var v = self.graphView.getNodeInReach(p, self.reachRadius);
+				if (v === undefined) {
+					self.graphView.addNode(p[0], p[1]);
+				}
 			})
 			.on("drag", function(d,i) {
 				var p = d3.mouse(self.svg.node());
@@ -102,7 +109,7 @@ function ViewManager(look) {
 				
 				if (self.dragcount > 1) {
 					console.log("multitouch");
-					
+					// would this be a drag gesture?
 				} else {
 					if (self.dragcount != 1) console.log("wrong dragcount?");
 					console.log("move vertex (if exisiting)");
