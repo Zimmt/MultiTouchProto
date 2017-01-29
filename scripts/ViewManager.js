a1ad6842127c8cf8ebbdd856dee291a3468edfca
Zimbel <nina.zimbel@web.de>
2017-01-29 17:32:16 +0100

    delete unnecessary code

--- scripts/ViewManager.js
+++ scripts/ViewManager.js
@@ -1,12 +1,9 @@
 
 function InteractionView(svg, look) {
+	
 	this.svg = svg;
 	this.look = look;
 	this.trajectories = this.svg.append("g").attr("id", "trajectories");
-
-	var line_func = d3.svg.line()
-		.x(function(d) { return d.x; })
-		.y(function(d) { return d.y; });
 
 	this.enterTrajactory= function(path) {
 		var self = this;
@@ -23,7 +20,6 @@ function InteractionView(svg, look) {
 	}
 
 	this.updateTrajactory = function(path) {
-		//console.log("------------");
 		var t = this.trajectories.selectAll("path").data([path], function(p) { return p.id; });
 		t.attr("d", line_func(path.points)).style(this.look.traj.style.normal);
 	}
@@ -31,6 +27,7 @@ function InteractionView(svg, look) {
 
 
 function GraphView(svg, look) {
+	
 	this.svg = svg;
 	this.look = look;
 	this.nodes = this.svg.append("g").attr("id", "nodes");
@@ -59,25 +56,22 @@ function GraphView(svg, look) {
 			return (Math.abs(pos[0] - v.x) < rr && Math.abs(pos[1] - v.y) < rr);
 		}, this);
 	}
-	
-	
-
 }
 
 
-
-
 function ViewManager(look) {
-	var self = this;
 	
+	var self = this;
 	this.look = look;
+	
 	this.svg = d3.select("#graph").append("svg")
 		.attr("id", "canvas")
 		.attr(this.look.canvas.attr)
 		.style(this.look.canvas.style)
+		
 		.on("click", function(d,i) {
 			console.log("click");
-			d3.event.sourceEvent.stopPropagation();
+			d3.event.stopPropagation();
 			
 			var p = d3.mouse(self.svg.node());
 			var v = self.graphView.getNodeInReach(p, self.reachRadius);
@@ -86,10 +80,12 @@ function ViewManager(look) {
 				self.graphView.addNode(p[0], p[1]);
 			}
 		})
+		
 		.on("MozMagnifyGestureStart", function(d,i) {
 			d3.event.preventDefault();
 			d3.event.stopPropagation();
 		})
+		
 		.on("MozMagnifyGestureUpdate", function(d,i) {
 			d3.event.preventDefault();
 			d3.event.stopPropagation();
@@ -107,11 +103,13 @@ function ViewManager(look) {
 	this.reachRadius = 20;
 
 	this.drag = d3.behavior.drag()
+	
 			.on("dragstart", function(d,i) {
 				var p = d3.mouse(self.svg.node());
 				console.log("start " + p[0] + " " + p[1] );
 				self.dragcount++;
 			})
+			
 			.on("drag", function(d,i) {
 				var p = d3.mouse(self.svg.node());
 				//console.log("drag " + p[0] + " " + p[1] );
@@ -131,6 +129,7 @@ function ViewManager(look) {
 					}
 				}
 			})
+			
 			.on("dragend", function(d,i) {
 				var p = d3.mouse(self.svg.node());
 				console.log("end " + p[0] + " " + p[1] );
@@ -139,5 +138,4 @@ function ViewManager(look) {
 			});
 
 		this.svg.call(this.drag);
-
 }
