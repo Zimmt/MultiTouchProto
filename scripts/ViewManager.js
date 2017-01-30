91d315a528909e2170345531e65358b491fc1382
Zimbel <nina.zimbel@web.de>
2017-01-30 12:34:28 +0100

    add distinction of cases for double touch

--- scripts/ViewManager.js
+++ scripts/ViewManager.js
@@ -31,48 +31,28 @@ function GraphView(svg, look) {
 	
 	this.svg = svg;
 	this.look = look;
-	this.edges = this.svg.append("g").attr("id", "edges");
 	this.nodes = this.svg.append("g").attr("id", "nodes");
+	this.nodeData = [];
 
-	var line_func = d3.line()
-		.x(function(d) { return d.x; })
-		.y(function(d) { return d.y; });
-	
-	this.enterGraph = function(graph) {
-		this.enterEdges(graph.edges);
-		this.enterVertices(graph.vertices);	
-	}
-
-	this.enterVertices = function(vertices) {
-		var self = this;
-		var t = this.nodes.selectAll("circle")
-			.data(vertices, function(v) { return v.id})
-			.attr(this.look.node.attr.normal);
-
-		var nodes = t.enter()
+	this.addNode = function(x,y) {
+		this.nodeData.push({id: this.nodeData.length, x: x, y: y});
+		this.nodes.selectAll("circle").data(this.nodeData, function(v) {return v.id;})
+			.enter()
 			.append("circle")
-			.attr(this.look.node.attr.init)
-			.style(this.look.node.style.init)
-			.transition("nodes").duration(150)
-			.attr(this.look.node.attr.normal)
-			.style(this.look.node.style.normal);
+			.attrs(this.look.node.attr.normal)
+			.styles(this.look.node.style.normal);
+	}
+	
+	this.updateNodes = function() {
+		this.nodes.selectAll("circle").data(this.nodeData, function(v) {return v.id;})
+		.attrs(this.look.node.attr.normal)
+		.styles(this.look.node.style.normal);
 	}
 
-	this.enterEdges = function(edges) {
-		var self = this;
-		var t = this.edges.selectAll("path").data(edges, function(e) { return e.id});
-		//t.enter()
-		//    .append("path")
-		//    .attr("d", line_func(path.points))
-		//    .style(this.look.traj.style.normal);
-		t = t.attr(this.look.edge.attr);
-		var edges = t.enter()
-			.append("path")
-			//.attr("d", line_func(path))
-			.attr(this.look.edge.attr)
-			.style(this.look.edge.style.init)
-			.transition("edges").duration(150)
-			.style(this.look.edge.style.normal);
+	this.getNodeInReach = function(pos, rr) {
+		return this.nodeData.find(function(v) {
+			return (Math.abs(pos[0] - v.x) < rr && Math.abs(pos[1] - v.y) < rr);
+		}, this);
 	}
 }
 
@@ -90,15 +70,88 @@ function ViewManager(look) {
 	this.interactionView = new InteractionView(this.svg, this.look);
 	this.graphView = new GraphView(this.svg, this.look);
 	
+	this.doubleGesture = undefined;
+	
 	this.dragcount = 0;
+	this.dragEps = 5;
 	
 	this.singleDrag = function(d, mousePos) {
 		console.log("singleDrag");
+		var p = d3.mouse(self.svg.node());
+		
+		var v = self.graphView.getNodeInReach(p, self.reachRadius);
+		if (v !== undefined) {
+			console.log("move vertex");
+			v.x += d3.event.dx;
+	  	v.y += d3.event.dy; 
+			self.graphView.updateNodes();
+		}
 	}
 	
 	this.doubleDrag = function(d, mousPos) {
 		console.log("doubleDrag");
+		var p = d3.mouse(self.svg.node());
+		
+		if (d3.event.dx < self.dragEps && d3.event.dy < self.dragEps) { // fix position
+			if (self.doubleGesture === undefined) {
+				self.doubleGesture = {name: "fix", pos: p, id: d3.event.identifier};
+				
+			} else if (self.doubleGesture.name == "fix" 
+				&& self.doubleGesture.id != d3.event.identifier) {
+					self.doubleGesture = {name: "doubleFix", 
+						pos1: self.doubleGesture.pos, id1: self.doubleGesture,
+						pos2: p, id2: d3.event.identifier};
+				
+			} else if (self.doubleGesture.name = "move"
+				&& self.doubleGesture.id != d3.event.identifier) {
+					self.doubleGesture = {name: "rotate", 
+						center: p, idc: d3.event.identifier,
+						rotPos: self.doubleGesture.pos, idr: self.doubleGesture.id};
+			
+			} // for doubleFix, doubleMove or rotate 
+			  //nothing is to do as the position didn't change that much
+			
+		} else { // moving position
+			if (self.doubleGesture === undefined) {
+				self.doubleGesture = {name: "move", pos: p, id: d3.event.identifier};
+				
+			} else if (self.doubleGesture.name == "fix") {
+				if (self.doubleGesture.id != d3.event.identifier) {
+						self.doubleGesture = {name: "rotate", 
+							center: self.doubleGesture.pos, idc: self.doubleGesture,
+							rotPos: p, idr: d3.event.identifier};
+				} else {
+					self.doubleGesture.name = "move";
+					self.doubleGesture.pos = p;
+				}
+				
+			} else if (self.doubleGesture.name = "move"
+				&& self.doubleGesture.id != d3.event.identifier) {
+					self.doubleGesture = {name: "doubleMove", 
+						pos1: p, id1: d3.event.identifier,
+						pos2: self.doubleGesture.pos, id2: self.doubleGesture.id};
+						
+			} else if (self.doubleGesture.name = "doubleFix") {
+				if (self.doubleGesture.id1 == d3.event.identifier) {
+					self.doubleGesture = {name: "rotate",
+						center: self.doubleGesture.pos2, idc: self.doubleGesture.id2,
+						rotPos: p, idr: d3.event.identifier};
+						
+				} else if (self.doubleGesture.id2 == d3.event.identifier) {
+					self.doubleGesture = {name: "rotate",
+						center: p, idc: d3.event.identifier,
+						rotPos: self.doubleGesture.pos2, idr: self.doubleGesture.id2};
+						
+				} else {
+					console.log("There is something wrong with the identifiers");
+				}
+			}
+		}
+		console.log(self.doubleGesture.name);
 	}
+	
+	this.dragcount = 0;
+	this.reachRadius = 20;
 
 	this.drag = d3.drag()
 	
@@ -110,12 +163,19 @@ function ViewManager(look) {
 			})
 	
 			.on("start", function(d,i) {
-				console.log("start " + d3.event.identifier);
+				//console.log("start " + d3.event.identifier);
 				self.dragcount = d3.event.active + 1; // d3.event has exposes several useful fields...
+
+				var p = d3.mouse(self.svg.node());
+				var v = self.graphView.getNodeInReach(p, self.reachRadius);
+				if (v === undefined) {
+					console.log("add node at pos " + p[0] + " " + p[1]);
+					self.graphView.addNode(p[0], p[1]);
+				}
 			})
 			
 			.on("drag", function(d,i) {
-				console.log("drag " + d3.event.type);
+			//	console.log("drag " + d3.event.type);
 				if (self.dragcount == 1) {
 					self.singleDrag(d, d3.mouse);
 				} else if (self.dragcount == 2) {
